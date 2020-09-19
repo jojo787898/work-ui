1ee78685816c4f457dac933f5be1104ff42ff105
yohanes koeswono <y.koeswono2311@gmail.com>
2020-09-19 13:26:29 -0700

    tambahanin lg

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import { getWork } from "./api/report";
-import Paper from "@material-ui/core"
+import {
+    Paper,
+    Grid,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow
+} from "@material-ui/core"
 
 export default class HomePage extends React.Component {
 
@@ -30,14 +39,50 @@ export default class HomePage extends React.Component {
 
     renderWork(work) {
         return (
-            <h1>{work.workOrder}</h1>
+            <TableRow>
+                <TableCell > {work.workOrder}</TableCell>
+                <TableCell align="right"> {work.facility}</TableCell>
+                <TableCell align="right"> Pump </TableCell>
+                <TableCell align="right"> {work.equipmentId}</TableCell>
+                <TableCell align="right"> {work.priority}</TableCell>
+                <TableCell align="right"> {work.timeComplete} </TableCell>
+                <TableCell align="right"> {work.submission} </TableCell>
+            </TableRow>
         )
     }
 
-    render() {
-        return (
+    renderItemDetail() {
+        <div>
+            
+        </div>
+    }
 
-                <h1> {this.renderSchedule()} </h1>
+    render() {
+
+        return (
+            <form className="homePage" style={{backgroundColor:"white"}}>
+
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell> Work Order </TableCell>
+                            <TableCell align="right"> Facility </TableCell>
+                            <TableCell align="right"> Equipment Type </TableCell>
+                            <TableCell align="right"> Equipment Id </TableCell>
+                            <TableCell align="right"> Priority </TableCell>
+                            <TableCell align="right"> Time to Complete </TableCell>
+                            <TableCell align="right"> Submission Timestamp </TableCell>
+                        </TableRow>
+                    </TableHead>
+
+                    <TableBody>
+                        {this.renderSchedule()}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            </form>
+            
         );
     }
 
